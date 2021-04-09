7d6c0fc9b69f068dce66959f1eab10a7b89a99fa
eman hamid <emanhharki@gmail.com>
2021-04-09 15:14:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
-let text =document.getElementById('');
+let text =document.getElementById('text');
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
