f0252119425199b990dd141caba8eba05b2a9a4f
eman hamid <emanhharki@gmail.com>
2021-04-09 15:21:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
- document.getElementById("text").innerHTML = "The DOM has loaded";
+ document.getElementById("text").innerHTML = "";
 });
 // console.log(
 //   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
