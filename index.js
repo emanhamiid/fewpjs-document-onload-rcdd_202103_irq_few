220c4c6943678c681f18cb64266fc22af6f9c240
eman hamid <emanhharki@gmail.com>
2021-04-09 15:14:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let text =document.getElementById('text');
 document.addEventListener("DOMContentLoaded", function() {
 
-  text.innerText=("The DOM has loaded")
+  text.innerText=("The DOM has loaded");
   console.log("The DOM has loaded");
 });
 // console.log(
